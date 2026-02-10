1424afabfddb07f0304fcb959fd4dba9b4952a75
leejaeuk0718 <jaeuk2210@naver.com>
2026-02-10 16:26:24 +0900

    first commit

---
+++ src/main/java/com/busabit501/server_test1_0210/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.busabit501.server_test1_0210;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
